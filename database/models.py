1778e76b3ba2394d99df007ee1aeb572f549a4c8
Vlad <rejoller@gmail.com>
2024-07-16 15:08:39 +0700

    добавлена запись данных в таблицу Fires

--- database/models.py
+++ database/models.py
@@ -1,13 +1,10 @@
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
-from sqlalchemy import ForeignKey, String, BIGINT, TIMESTAMP, DateTime, BOOLEAN
+from sqlalchemy import ForeignKey, String, BIGINT, TIMESTAMP, DateTime, BOOLEAN, FLOAT, NUMERIC, INTEGER
 
 class Base(DeclarativeBase):
     pass
 
 
-class Fires(Base):
-    __tablename__ = 'fires'
-    fire_id: Mapped[int] = mapped_column(BIGINT, primary_key=True, autoincrement=True)
 
 class Messages(Base):
     __tablename__ = 'messages'
@@ -48,3 +45,42 @@ class Users(Base):
     msg_type: Mapped[int] = mapped_column(BIGINT)
 
 
+
+class Fires(Base):
+    __tablename__ = 'fires'
+    fire_id: Mapped[int] = mapped_column(BIGINT, primary_key=True, autoincrement=True)
+    date_detect: Mapped[DateTime] = mapped_column(TIMESTAMP)
+    date_terminate: Mapped[DateTime] = mapped_column(TIMESTAMP, nullable=True)
+    date_actual: Mapped[DateTime] = mapped_column(TIMESTAMP)
+    date_import: Mapped[DateTime] = mapped_column(TIMESTAMP)
+    fire_ext_id: Mapped[str] = mapped_column(String(255))
+    fire_num: Mapped[int] = mapped_column(INTEGER)
+    fire_zone: Mapped[str] = mapped_column(String(255))
+    map_id: Mapped[str] = mapped_column(String(255))
+    region: Mapped[str] = mapped_column(String(255))
+    forestry_id: Mapped[int] = mapped_column(INTEGER)
+    forestry_name: Mapped[str] = mapped_column(String(255))
+    city: Mapped[str] = mapped_column(String(255))
+    azimuth: Mapped[int] = mapped_column(INTEGER)
+    distance: Mapped[int] = mapped_column(INTEGER)
+    fire_status: Mapped[str] = mapped_column(String(255))
+    forces_aps: Mapped[int] = mapped_column(INTEGER)
+    forces_lps: Mapped[int] = mapped_column(INTEGER)
+    forces_: Mapped[int] = mapped_column(INTEGER)
+    forces_rent: Mapped[int] = mapped_column(INTEGER)
+    forces_mchs: Mapped[int] = mapped_column(INTEGER)
+    fire_area: Mapped[float] = mapped_column(FLOAT)
+    ext_log: Mapped[int] = mapped_column(INTEGER)
+    email_id: Mapped[str] = mapped_column(String(255))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- utils/result_df_maker.py
+++ utils/result_df_maker.py
@@ -4,25 +4,8 @@ import pandas as pd
 
 async def result_df_maker(df, df_2):
 
-    date_format = '%d.%m.%Y %H:%M:%S'
+    
     result_df = df.merge(df_2, left_on='ID Карты', right_on='map_id')
-    try:
-        result_df[['Дата ликвидации пожара', 'Дата изменения данных', 'Актуальность данных', 'Дата возникновения пожара']] = result_df[['Дата ликвидации пожара', 'Дата изменения данных', 'Актуальность данных', 'Дата возникновения пожара']]\
-            .apply(pd.to_datetime, format=date_format, dayfirst=True, errors='coerce')
-
-        result_df[['Дата ликвидации пожара', 'Дата изменения данных', 'Актуальность данных', 'Дата возникновения пожара']] = result_df[['Дата ликвидации пожара', 'Дата изменения данных', 'Актуальность данных', 'Дата возникновения пожара']]\
-            .apply(lambda x: x.dt.strftime('%d.%m %H:%M'))
-    except (KeyError, IndexError, AttributeError) as e:
-        logging.error(f"ошибка при форматировании дат {e}")
-        
-        
-    try:
-        result_df[['Дата ликвидации', 'Актуально', 'Дата обнаружения']] = result_df[['Дата ликвидации', 'Актуально', 'Дата обнаружения']]\
-            .apply(pd.to_datetime, format=date_format, dayfirst=True, errors='coerce')
-
-        result_df[['Дата ликвидации', 'Актуально', 'Дата обнаружения']] = result_df[['Дата ликвидации', 'Актуально', 'Дата обнаружения']]\
-            .apply(lambda x: x.dt.strftime('%d.%m %H:%M'))
-    except (KeyError, IndexError, AttributeError) as e:
-        logging.error(f"ошибка при форматировании дат {e}")
+    
         
     return pd.DataFrame(result_df)

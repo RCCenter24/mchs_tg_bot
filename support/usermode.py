773210fe1a66d8e887c76a742b4942ba668aa044
Vlad <rejoller@gmail.com>
2024-08-22 15:22:31 +0700

    исправлен путь импорта в usermode

--- support/usermode.py
+++ support/usermode.py
@@ -2,7 +2,7 @@ from aiogram import Router, Bot, F
 from aiogram.filters import StateFilter
 from aiogram import types
 from aiogram.fsm.context import FSMContext
-from handlers import Form
+from users.user_states import Form
 from config import admin_group_chat_id
 from sqlalchemy.ext.asyncio import AsyncSession
 

145ac0ce1e7dde1852380552481a812c459dc1c3
rejoller <130197750+rejoller@users.noreply.github.com>
2024-09-27 12:26:53 +0700

    изменено описание стартовой команды, переезд на нового бота

--- handlers/start_command.py
+++ handlers/start_command.py
@@ -5,7 +5,6 @@ from aiogram.filters import CommandStart
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
@@ -13,11 +12,10 @@ from user_manager import UserManager
 from images import main_photo
 
 
-
 router = Router()
 
 
-@router.message(CommandStart(), F.chat.type == 'private')
+@router.message(CommandStart(), F.chat.type == "private")
 async def handle_start(message: Message, state: FSMContext, session: AsyncSession):
     await state.clear()
     user_manager = UserManager(session)
@@ -26,18 +24,28 @@ async def handle_start(message: Message, state: FSMContext, session: AsyncSessio
 
     builder = InlineKeyboardBuilder()
 
-    markup = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(
-            text='Выбрать муниципальное образование', callback_data='choise_munic')],
-        [InlineKeyboardButton(
-            text='Подписаться на все обновления', callback_data='choise_all_munic')]
-    ])
+    markup = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="Выбрать муниципальное образование",
+                    callback_data="choise_munic",
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="Подписаться на все обновления",
+                    callback_data="choise_all_munic",
+                )
+            ],
+        ]
+    )
 
     builder.adjust(1)
     builder.attach(InlineKeyboardBuilder.from_markup(markup))
 
-    caption = ("Это бот по инцидентам МЧС Красноярского края. Чтобы подписаться на одно из муниципальных "
-               "образований для получения новостей воспользуйтесь командой /subscribe \n Чтобы подписаться "
-               "на все обновления нажмите на команду /subscribe_all\n")
+    caption = ("Что умеет этот бот?\n\nБот «И 1 Лесные пожары» позволяет получить общую "
+               "статистическую информацию по ряду основных параметров, характеризующих "
+               "текущую лесопожарную обстановку за сутки, а также незамедлительно при обнаружении пожара.")
 
     await message.answer_photo(caption=caption, reply_markup=markup, photo=main_photo)

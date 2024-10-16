88c718f81fc6ac4dc97811ae8eb7d12241af5174
rejoller <130197750+rejoller@users.noreply.github.com>
2024-10-16 16:43:14 +0700

    закомменчен шедулер

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ async def main():
     dp.message.middleware(LoggingMiddleware())
     dp.message.middleware(UsersMiddleware())
     scheduler = AsyncIOScheduler(timezone=ZoneInfo("Asia/Krasnoyarsk"))
-    scheduler.add_job(on_startup, 'interval', minutes=interval_min)
+    # scheduler.add_job(on_startup, 'interval', minutes=interval_min)
     # scheduler.add_job(daily_report_sender, 'cron', hour= 9, minute=30, day_of_week = 'mon-fri')
     scheduler.start()
     print('Бот запущен и готов к приему сообщений')
